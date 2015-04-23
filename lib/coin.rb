8b53ae45959e3beb29dcfbfa71519364907de842
Jim Patt & andrew david burt <student@epicodus.com>
2015-04-23 10:43:25 -0700

    first pass on quarters

--- lib/coin.rb
+++ lib/coin.rb
@@ -1,5 +1,9 @@
-class String
+class Fixnum
   define_method(:coin) do
-
+    output = ""
+    quarters = self./(25).floor()
+    output = quarters.to_s().concat(" quarters")    
+#    pennies = self.%(1).to_s()
+#    output = pennies.concat(" pennies")
   end
 end
--- spec/coin_spec.rb
+++ spec/coin_spec.rb
@@ -2,12 +2,17 @@ require('rspec')
 require('coin')
 require('pry')
 
-describe('String#coin') do
+describe('Fixnum#coin') do
+
   it("given a number of cents, computes smallest change") do
-    expect(3.coin()).to(eq("3 pennies"))
+    expect(75.coin()).to(eq("3 quarters"))
   end
 
   it("given a number of cents, computes smallest change") do
     expect(92.coin()).to(eq("3 quarters, 1 dime, 1 nickle, 2 pennies"))
+  end
+
+  it("given a number of cents, computes smallest change") do
+    expect(3.coin()).to(eq("3 pennies"))
   end
 end
