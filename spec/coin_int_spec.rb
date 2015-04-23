21153dc0280520de102ca879be977d61f3038718
Jim Patt & andrew david burt <student@epicodus.com>
2015-04-23 10:22:04 -0700

    Fix syntax

--- spec/coin_int_spec.rb
+++ spec/coin_int_spec.rb
@@ -2,7 +2,7 @@ require ('capybara/rspec')
 require ('./app')
 Capybara.app = Sinatra::Application
 
-describe('given a number of cents, computes smallest change'), {:type => :feature}) do
+describe('given a number of cents, computes smallest change', {:type => :feature}) do
   it("given a number of cents, computes smallest change") do
     visit('/')
     fill_in('cents' , :with => 3)
@@ -11,10 +11,10 @@ describe('given a number of cents, computes smallest change'), {:type => :featur
   end
 end
 
-describe('given a number of cents, computes smallest change'), {:type => :feature}) do
+describe('given a number of cents, computes smallest change', {:type => :feature}) do
   it("given a number of cents, computes smallest change") do
     visit('/')
-    fill_in('cents') , :with => 92)
+    fill_in('cents' , :with => 92)
     click_button('submit')
     expect(page).to have_content('3 quarters, 1 dime, 1 nickle, 2 pennies')
   end
