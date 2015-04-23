a0a712e122a9797d97ee1f581ebe318481a3e82e
Jim Patt & andrew david burt <student@epicodus.com>
2015-04-23 10:51:48 -0700

    first rspec

--- lib/coin.rb
+++ lib/coin.rb
@@ -2,7 +2,16 @@ class Fixnum
   define_method(:coin) do
     output = ""
     quarters = self./(25).floor()
-    output = quarters.to_s().concat(" quarters")    
+    remainder = self.%(25)
+    output = quarters.to_s().concat(" quarters, ")
+    dimes = remainder./(10).floor()
+    remainder = remainder.%(10)
+    output = output.concat(dimes.to_s().concat(" dimes, "))
+    nickels = remainder./(5).floor()
+    remainder = remainder.%(5)
+    output = output.concat(nickels.to_s().concat(" nickels, "))
+    pennies = remainder
+    output = output.concat(pennies.to_s().concat(" pennies"))
 #    pennies = self.%(1).to_s()
 #    output = pennies.concat(" pennies")
   end
--- spec/coin_spec.rb
+++ spec/coin_spec.rb
@@ -9,7 +9,7 @@ describe('Fixnum#coin') do
   end
 
   it("given a number of cents, computes smallest change") do
-    expect(92.coin()).to(eq("3 quarters, 1 dime, 1 nickle, 2 pennies"))
+    expect(92.coin()).to(eq("3 quarters, 1 dimes, 1 nickels, 2 pennies"))
   end
 
   it("given a number of cents, computes smallest change") do
